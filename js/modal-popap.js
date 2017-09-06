7c37181a8af5f3fd7f2712ac86a2ed7bb3ba1633
Dmitry <soyaburritos@gmail.com>
2017-09-06 20:51:20 +0300

    Добавил js для popUp

--- js/modal-popap.js
+++ js/modal-popap.js
@@ -1,7 +1,14 @@
 var writeLink = document.querySelector(".write-button");
 var modalPopup = document.querySelector(".modal-writeus");
 
+var closeLink = document.querySelector(".modal-close");
+
 writeLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalPopup.classList.add("modal-show");
   });
+
+closeLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalPopup.classList.remove("modal-show");
+});
