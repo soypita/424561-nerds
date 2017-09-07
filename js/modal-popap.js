18a039507511aadd226bf1ca659da74e4fd55e3b
Dmitry <soyaburritos@gmail.com>
2017-09-07 22:40:10 +0300

    Последние штрихи

--- js/modal-popap.js
+++ js/modal-popap.js
@@ -4,15 +4,53 @@ var modalPopup = document.querySelector(".modal-writeus");
 
 var closeLink = modalPopup.querySelector(".modal-close");
 
-var userName = modalPopup.querySelector("[name=write-name]");
+var writeName = modalPopup.querySelector("[name=write-name]");
+
+var writeEmail = modalPopup.querySelector("[name=write-email]");
+
+var writeMessage = modalPopup.querySelector("[name=write-message]");
+
+var writeForm = modalPopup.querySelector(".login-form");
+
+var storageName = localStorage.getItem("writeName");
+
+var storageEmail = localStorage.getItem("writeEmail");
 
 writeLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalPopup.classList.add("modal-show");
-  userName.focus();
+  if(storageName) {
+    writeName.value = storageName;
+  }
+  if(storageEmail) {
+    writeEmail.value = storageEmail;
+  }
+  storageName ? (storageEmail ? writeMessage.focus() : writeEmail.focus()) : writeName.focus();
   });
 
 closeLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalPopup.classList.remove("modal-show");
+  modalPopup.classList.remove("modal-error");
+});
+
+writeForm.addEventListener("submit", function (evt) {
+  if(!writeName.value || !writeEmail.value || !writeMessage.value) {
+    evt.preventDefault();
+    modalPopup.classList.remove("modal-error");
+    modalPopup.offsetWidth = modalPopup.offsetWidth;
+    modalPopup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("writeName", writeName.value);
+    localStorage.setItem("writeEmail", writeEmail.value);
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if(evt.keyCode === 27) {
+    if (modalPopup.classList.contains("modal-show")) {
+      modalPopup.classList.remove("modal-show");
+      modalPopup.classList.remove("modal-error");
+    }
+  }
 });
