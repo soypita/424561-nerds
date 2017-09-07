b0a6af2ed77707a15ecaffa4974b99dd8d141932
Dmitry <soyaburritos@gmail.com>
2017-09-07 21:44:03 +0300

    Поправил верстку и скрипт

--- js/modal-popap.js
+++ js/modal-popap.js
@@ -1,11 +1,15 @@
 var writeLink = document.querySelector(".write-button");
+
 var modalPopup = document.querySelector(".modal-writeus");
 
-var closeLink = document.querySelector(".modal-close");
+var closeLink = modalPopup.querySelector(".modal-close");
+
+var userName = modalPopup.querySelector("[name=write-name]");
 
 writeLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalPopup.classList.add("modal-show");
+  userName.focus();
   });
 
 closeLink.addEventListener("click", function (evt) {
