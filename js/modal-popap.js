58de00422ce0fd50e0de3e66d9b88a5ca22bf999
Dmitry <soyaburritos@gmail.com>
2017-09-08 22:09:55 +0300

    Последнее

--- js/modal-popap.js
+++ js/modal-popap.js
@@ -12,23 +12,11 @@ var writeMessage = modalPopup.querySelector("[name=write-message]");
 
 var writeForm = modalPopup.querySelector(".login-form");
 
-var storageName = localStorage.getItem("writeName");
-
-var storageEmail = localStorage.getItem("writeEmail");
-
 writeLink.addEventListener("click", function(evt)
 {
   evt.preventDefault();
   modalPopup.classList.add("modal-show");
-  if (storageName)
-  {
-    writeName.value = storageName;
-  }
-  if (storageEmail)
-  {
-    writeEmail.value = storageEmail;
-  }
-  storageName ? (storageEmail ? writeMessage.focus() : writeEmail.focus()) : writeName.focus();
+  writeName.focus();
 });
 
 closeLink.addEventListener("click", function(evt)
@@ -46,11 +34,6 @@ writeForm.addEventListener("submit", function(evt)
     modalPopup.classList.remove("modal-error");
     modalPopup.offsetWidth = modalPopup.offsetWidth;
     modalPopup.classList.add("modal-error");
-  }
-  else
-  {
-    localStorage.setItem("writeName", writeName.value);
-    localStorage.setItem("writeEmail", writeEmail.value);
   }
 });
 
